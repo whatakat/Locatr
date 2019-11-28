211428fa256d1eb3dd168ccf8fbe9bf41f2a87bb
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-11-28 21:45:30 +0300

    add menu in fragment

--- app/src/main/java/com/example/locatr/LocatrFragment.java
+++ app/src/main/java/com/example/locatr/LocatrFragment.java
@@ -2,6 +2,8 @@ package com.example.locatr;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -17,11 +19,23 @@ public class LocatrFragment extends Fragment {
         return new LocatrFragment();
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_locatr,container, false);
         mImageView = (ImageView) v.findViewById(R.id.image);
         return v;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.fragment_locatr, menu);
     }
 }
