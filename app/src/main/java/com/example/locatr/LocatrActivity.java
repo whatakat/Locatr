d56e7425c902021e1372b9755d75435eeaf0db85
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-11-29 23:20:41 +0300

    test play-service-location

--- app/src/main/java/com/example/locatr/LocatrActivity.java
+++ app/src/main/java/com/example/locatr/LocatrActivity.java
@@ -1,11 +1,38 @@
 package com.example.locatr;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+
 import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 
 public class LocatrActivity extends SingleFragmentActivity {
+    private static final int REQUEST_ERROR = 0;
+
     @Override
     protected Fragment createFragment() {
         return LocatrFragment.newInstance();
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        int errorCode = apiAvailability.isGooglePlayServicesAvailable(this);
+        if (errorCode != ConnectionResult.SUCCESS){
+            Dialog errorDialog = apiAvailability.
+                    getErrorDialog(this, errorCode, REQUEST_ERROR,
+                            new DialogInterface.OnCancelListener() {
+                                @Override
+                                public void onCancel(DialogInterface dialog) {
+                                    finish();
+                                }
+                            });
+            errorDialog.show();
+        }
+    }
 }
+
