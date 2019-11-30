bf7c93812a4b6470c77a5d2557e1fd8b212aa994
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-11-30 22:15:36 +0300

    query building

--- app/src/main/java/com/example/locatr/LocatrFragment.java
+++ app/src/main/java/com/example/locatr/LocatrFragment.java
@@ -14,6 +14,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
 public class LocatrFragment extends Fragment {
@@ -71,5 +72,11 @@ public class LocatrFragment extends Fragment {
         inflater.inflate(R.menu.fragment_locatr, menu);
         MenuItem searchItem = menu.findItem(R.id.action_locate);
         searchItem.setEnabled(mClient.isConnected());
+    }
+    private void findImage(){
+        LocationRequest request = LocationRequest.create();
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        request.setNumUpdates(1);
+        request.setInterval(0);
     }
 }
