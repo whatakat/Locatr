1dce70495402cc0cce779faa08097d9bfe117721
Leoneed <40763564+whatakat@users.noreply.github.com>
2019-11-30 22:23:02 +0300

    LocationServices

--- app/src/main/java/com/example/locatr/LocatrFragment.java
+++ app/src/main/java/com/example/locatr/LocatrFragment.java
@@ -1,6 +1,8 @@
 package com.example.locatr;
 
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -14,10 +16,12 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
 public class LocatrFragment extends Fragment {
+    private static final String TAG  = "LocatrFragment";
     private ImageView mImageView;
     private GoogleApiClient mClient;
 
@@ -78,5 +82,12 @@ public class LocatrFragment extends Fragment {
         request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
         request.setNumUpdates(1);
         request.setInterval(0);
+        LocationServices.FusedLocationApi
+                .requestLocationUpdates(mClient, request, new LocationListener() {
+                    @Override
+                    public void onLocationChanged(Location location) {
+                        Log.i(TAG, "Got a fix: "+ location);
+                    }
+                });
     }
 }
